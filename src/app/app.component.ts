286bf41276d2557e6637e480964c5ceeb8840fd7
guneyeroglu <benguneyeroglu@gmail.com>
2024-01-28 19:25:25 +0300

    #update: structure changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'rent-house-ng';
+  title = 'Rent House Application Website';
 }
