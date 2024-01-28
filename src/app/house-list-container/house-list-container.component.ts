df023f9e91810d0c3278ff7be0d146481120cad0
guneyeroglu <benguneyeroglu@gmail.com>
2024-01-28 19:22:49 +0300

    #create: new comp

---
+++ src/app/house-list-container/house-list-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-house-list-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './house-list-container.component.html',
+  styleUrl: './house-list-container.component.scss',
+})
+export class HouseListContainerComponent {}
