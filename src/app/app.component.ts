d530d85492437e02151978c48040c737dc64b8fa
guneyeroglu <benguneyeroglu@gmail.com>
2024-02-10 19:56:16 +0300

    #update: naming, scss, route changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hr-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent, DetailsComponent } from './pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    title: 'Rent House - Home',
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+    title: 'Rent House - House Detail',
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+  },
+];
